51b7fc548ad5a0d42ad37fc01b3e684d4335c634
Seow Yong Tao <seowyongtao22@gmail.com>
2023-04-03 21:33:04 +0800

    add new folder to store old codes

--- old/automate_finsentiment_analysis.py
+++ old/automate_finsentiment_analysis.py
@@ -81,14 +81,14 @@ for company in companies:
 
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
-            with open('../tmp.pdf', 'wb') as f:
+            with open('tmp.pdf', 'wb') as f:
                 for chunk in r.iter_content(chunk_size=8192):
                     # If you have chunk encoded response uncomment if
                     # and set chunk_size parameter to None.
                     # if chunk:
                     f.write(chunk)
 
-        pdf = PdfReader('../tmp.pdf')
+        pdf = PdfReader('tmp.pdf')
 
         page_number = 2
         page_object = pdf.pages[page_number - 1]
--- old/download_pdf.py
+++ old/download_pdf.py
@@ -7,14 +7,14 @@ url = 'https://www.7eleven.com.my/pdf/ar-2017.pdf'
 # NOTE the stream=True parameter below
 with requests.get(url, stream=True) as r:
     r.raise_for_status()
-    with open('../tmp.pdf', 'wb') as f:
+    with open('tmp.pdf', 'wb') as f:
         for chunk in r.iter_content(chunk_size=8192):
             # If you have chunk encoded response uncomment if
             # and set chunk_size parameter to None.
             # if chunk:
             f.write(chunk)
 
-pdf = PdfReader('../tmp.pdf')
+pdf = PdfReader('tmp.pdf')
 
 page_number = 2
 page_object = pdf.pages[page_number - 1]
